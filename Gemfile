4d9defa9195f1dda1fbb12ade5b2b7ec99b12ee1
Rune Skjoldborg Madsen <rune@runemadsen.com>
2013-10-08 11:31:18 -0400

    passing tests

--- Gemfile
+++ Gemfile
@@ -2,5 +2,7 @@ source 'https://rubygems.org'
 
 group :test do
   gem "asciidoctor", "~> 0.1.4"
+  gem "nokogiri"
+  gem "tilt"
   gem "rspec"
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,3 +8,11 @@ RSpec.configure do |config|
   config.color_enabled = true
   config.order = "random"
 end
+
+def convert(asciidoc)
+  Asciidoctor.render(asciidoc, :template_dir => htmlbook_path)
+end
+
+def htmlbook_path
+  "#{File.dirname(__FILE__)}/../htmlbook"
+end
