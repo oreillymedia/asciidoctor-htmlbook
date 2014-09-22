8305e06525fee22c3484f4ba56fbc975d8614ccc
Jakub Jirutka <jakub@jirutka.cz>
2014-09-22 23:41:03 +0200

    fixing code-style: indentation, final newlines and trailing whitespaces

--- scripts/convert_passthroughs.rb
+++ scripts/convert_passthroughs.rb
@@ -47,13 +47,13 @@ class BlockDocBook2HTMLBookprocessor < Asciidoctor::Extensions::Treeprocessor
   def process
     process_blocks @document if @document.blocks?
   end
-   
+
   def process_blocks(node)
     node.blocks.each_with_index do |block, i|
       if block.respond_to? :context
         if block.context == :pass
-            block_text = convert_to_htmlbook(block.content)
-            node.blocks[i] = Asciidoctor::Block.new @document, :pass, :content_model => :raw, :subs => [], :source => block_text
+          block_text = convert_to_htmlbook(block.content)
+          node.blocks[i] = Asciidoctor::Block.new @document, :pass, :content_model => :raw, :subs => [], :source => block_text
         else
           process_blocks block if block.blocks?
         end
@@ -65,28 +65,28 @@ end
 
 
 def convert_to_htmlbook(text)
-    db2htmlbook = LibXML::XML::Document.file('/usr/local/app/docbook2htmlbook/db2htmlbook.xsl')
-    xslt = LibXSLT::XSLT::Stylesheet.new(db2htmlbook)
+  db2htmlbook = LibXML::XML::Document.file('/usr/local/app/docbook2htmlbook/db2htmlbook.xsl')
+  xslt = LibXSLT::XSLT::Stylesheet.new(db2htmlbook)
 
-    # Drop processing instructions
-    text = text.gsub(/\<\?(.*?)\?\>/, '')
+  # Drop processing instructions
+  text = text.gsub(/\<\?(.*?)\?\>/, '')
 
-    # If passthrough contains docbook content, apply xslt
-    if text.include?('<')
-      wrapped = "<root>" + text + "</root>"
-      passthrough = LibXML::XML::Parser.string(wrapped, :options => LibXML::XML::Parser::Options::RECOVER).parse
-      DOCBOOK_ELEMENTS.each { |d|
-        if passthrough.find_first('/root' + d)
-          result = xslt.apply(passthrough)
-          result = result.to_s.gsub(/\<\?xml version="1.0" encoding="UTF-8"\?\>\n/, '').gsub(/\<\/?root\>/, '')
-          return result
-        end
-        }
-    end
-
-    # Otherwise assume html and return string
-    return text
+  # If passthrough contains docbook content, apply xslt
+  if text.include?('<')
+    wrapped = "<root>" + text + "</root>"
+    passthrough = LibXML::XML::Parser.string(wrapped, :options => LibXML::XML::Parser::Options::RECOVER).parse
+    DOCBOOK_ELEMENTS.each { |d|
+      if passthrough.find_first('/root' + d)
+        result = xslt.apply(passthrough)
+        result = result.to_s.gsub(/\<\?xml version="1.0" encoding="UTF-8"\?\>\n/, '').gsub(/\<\/?root\>/, '')
+        return result
+      end
+    }
   end
+
+  # Otherwise assume html and return string
+  return text
+end
 
 
 Asciidoctor::Extensions.register do |document|
@@ -95,11 +95,11 @@ Asciidoctor::Extensions.register do |document|
 end
 
 Dir.glob("*.asc*") do |filename|
-    # For use on consolidated book file
-    if filename == "book.asciidoc" || filename == "book.asc"
-      Asciidoctor.render_file(filename, :safe => :safe, :in_place => true, :template_dir => "/usr/local/app/asciidoctor-htmlbook/htmlbook-autogen")
-    # For use on chunked files
-    else
-      Asciidoctor.render_file(filename, :safe => :safe, :in_place => true, :template_dir => "/usr/local/app/asciidoctor-htmlbook/htmlbook")
-    end
+  # For use on consolidated book file
+  if filename == "book.asciidoc" || filename == "book.asc"
+    Asciidoctor.render_file(filename, :safe => :safe, :in_place => true, :template_dir => "/usr/local/app/asciidoctor-htmlbook/htmlbook-autogen")
+  # For use on chunked files
+  else
+    Asciidoctor.render_file(filename, :safe => :safe, :in_place => true, :template_dir => "/usr/local/app/asciidoctor-htmlbook/htmlbook")
+  end
 end
